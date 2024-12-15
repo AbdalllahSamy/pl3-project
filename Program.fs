d4ef4fe2d743e1cb78ce65d5f40dadd41c9ca5d4
abdallllahsamy <abdalla.backend@utdsoftware.com>
2024-12-15 18:43:30 +0200

    first commit

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿namespace QuizApp
+
+open System.Windows.Forms
+open QuizApp.UI
+
+module Main =
+    [<EntryPoint>]
+    let main _ =
+        let form = QuizForm.createForm ()
+        Application.Run(form)
+        0
